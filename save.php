c1349b28b385ccc2ce1dfba7bfb8856a6a3f1451
beemoid <56728280+beemoid@users.noreply.github.com>
2022-05-14 13:44:50 +0700

    add txt file & function

--- save.php
+++ save.php
@@ -1,20 +1,20 @@
 <?php
-    if(isset($_POST['simpan'])) {
-        date_default_timezone_set('Asia/Jakarta');
+if (isset($_POST['simpan'])) {
+    date_default_timezone_set('Asia/Jakarta');
 
-        $txt = fopen('data.txt', 'w');
-        fwrite($txt, $_POST['nama'].' / '.$_POST['nim'].' / '.$_POST['positif'].' / '.$_POST['dirawat'].' / '.$_POST['sembuh'].' / '.$_POST['meninggal'].' / '.$_POST['wilayah'].' / '.date('d M Y H:i:s'));
-        fclose($txt);
+    $txt = fopen('data.txt', 'w');
+    fwrite($txt, 'Operator= ' . $_POST['nama'] . "\n" . "NIM= " . $_POST['nim'] ."\nWilayah= " . $_POST['wilayah'] . "\nWaktu= " . date('d M Y H:i:s'). "\n \n" . "Jumlah Positif= " . $_POST['positif'] . "\n" . "Jumlah Di Rawat" . $_POST['dirawat'] . "\n" . "Jumlah Sembuh= " . $_POST['sembuh'] . "\n" . "Jumlah Meninggal= " . $_POST['meninggal'] . "\n");
+    fclose($txt);
 
-        $fileName = 'db.html';
-        
-        $file = fopen($fileName, "w");
-        
-        $content = '
+    $fileName = 'db.html';
+
+    $file = fopen($fileName, "w");
+
+    $content = '
         <center>
-            <h1>Data Pemantauan Covid19 Wilayah '.$_POST['wilayah'].'</h1>
-            <h2>Per '.date('d M Y H:i:s').'</h2>
-            <h3>'.$_POST['nama'].' / '.$_POST['nim'].'</h3>
+            <h1>Data Pemantauan Covid19 Wilayah ' . $_POST['wilayah'] . '</h1>
+            <h2>Per ' . date('d M Y H:i:s') . '</h2>
+            <h3>' . $_POST['nama'] . ' / ' . $_POST['nim'] . '</h3>
         </center>
         <table class="table" width="100%" border="1">
             <thead>
@@ -27,17 +27,17 @@
             </thead>
             <tbody>
                 <tr>
-                    <td>'.$_POST['positif'].'</td>
-                    <td>'.$_POST['dirawat'].'</td>
-                    <td>'.$_POST['sembuh'].'</td>
-                    <td>'.$_POST['meninggal'].'</td>
+                    <td>' . $_POST['positif'] . '</td>
+                    <td>' . $_POST['dirawat'] . '</td>
+                    <td>' . $_POST['sembuh'] . '</td>
+                    <td>' . $_POST['meninggal'] . '</td>
                 </tr>
             </tbody>
         </table>
         ';
-        
-        fwrite($file, $content);
-        fclose($file);
-    }
 
-    header("location: tampil.php");
+    fwrite($file, $content);
+    fclose($file);
+}
+
+header("location: tampil.php");
